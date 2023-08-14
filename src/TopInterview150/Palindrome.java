74f15e9a9fad8cbad843f4bea47faff467cb3ea8
Chloe <owlgirlpnw@gmail.com>
2023-08-14 10:59:25 -0700

    Initial solution to isPalindrome from LeetCode.

---
+++ src/TopInterview150/Palindrome.java
@@ -0,0 +1,34 @@
+package TopInterview150;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("mom"));
+        System.out.println(isPalindrome("race a car"));
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+    }
+
+    public static boolean isPalindrome(String s) {
+        if (s.isEmpty()) return true;
+
+        int start = 0;
+        int last = s.length() - 1;
+
+        while (start <= last) {
+            char currFirst = s.charAt(start);
+            char currLast = s.charAt(last);
+
+            if (!Character.isLetterOrDigit(currFirst)) {
+                start++;
+            } else if (!Character.isLetterOrDigit(currLast)) {
+                last--;
+            } else {
+                if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+                    return false;
+                }
+                start++;
+                last--;
+            }
+        }
+        return true;
+    }
+}
